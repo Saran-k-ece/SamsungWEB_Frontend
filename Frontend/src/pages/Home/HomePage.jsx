a527dbc14e8b77c0eadad84d8e6453ac37a95e43
Saran-k-ece <saran.k2022ece@sece.ac.in>
2025-04-22 12:06:13 +0530

    New files are added

---
+++ Frontend/src/pages/Home/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomePage.css'
+import Header from '../../Components/Header/Header'
+import SubHeader from '../../Components/SubHeader/SubHeader'
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <SubHeader />
+    </div>
+  )
+}
+
+export default HomePage
